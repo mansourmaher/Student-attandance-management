38f800f89afa29cfc459ee4702ecea95c7b2695b
maher <mansourmaher77@gmail.com>
2026-04-24 23:31:29 +0100

    test lint error to cehck fail of lint jobs

--- server/middelware/authentication.js
+++ server/middelware/authentication.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+coddddnst jwt = require("jsonwebtoken");
 const Authentication = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader.split(" ")[1];
