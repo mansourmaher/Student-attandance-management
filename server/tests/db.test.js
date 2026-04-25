b8d200959abe461538903dd6801136893050b807
maher <mansourmaher77@gmail.com>
2026-04-25 15:12:09 +0100

    Add Jest and Supertest for testing; create initial MongoDB connection test
    - Updated package.json to include Jest and Supertest as dev dependencies.
    - Added a test script to run Jest with force exit option.
    - Created a new test file db.test.js to verify MongoDB connection.

---
+++ server/tests/db.test.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// eslint-disable-next-line no-undef
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGODB);
+});
+// eslint-disable-next-line no-undef
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+// eslint-disable-next-line no-undef
+test("MongoDB connection is successful", () => {
+  // eslint-disable-next-line no-undef
+  expect(mongoose.connection.readyState).toBe(1);
+});
